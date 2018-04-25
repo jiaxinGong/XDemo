e3b0b1fb0c98ca839b1854d06751f39901352458
zhangcs <1223895481@qq.com>
2018-04-25 19:27:07 +0800

    xlab

---
+++ jiaxin-xlab/src/main/java/com/jiaxin/xlab/web/controller/HelpController.java
@@ -0,0 +1,19 @@
+package com.jiaxin.xlab.web.controller;/**
+ * @Author: bufoon
+ * @Email: song.anling@zyxr.com
+ * @Datetime: Created In 2018/4/25 17:13
+ * @Desc: as follows.
+ */
+
+/**
+ * @description: 运营类
+ * @author: gongjiaxin
+ * @create: 2018-04-25 17:13
+ **/
+public class HelpController extends AbstractHelpController{
+
+    //获取风险信息
+    public String getRiskInfo() {
+        return null;
+    }
+}
